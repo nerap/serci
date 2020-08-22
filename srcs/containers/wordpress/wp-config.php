dcf605c8753bc78e6a16e56361970da51353f3b1
nerap <raph.en223@gmail.com>
2020-08-22 03:13:37 +0200

    i hate ft services

--- srcs/containers/wordpress/wp-config.php
+++ srcs/containers/wordpress/wp-config.php
@@ -32,16 +32,14 @@ define( 'DB_PASSWORD', 'motdepasse' );
 /** MySQL hostname */
 define( 'DB_HOST', 'mysql');
 
-define('WP_HOME', 'http://REPLACEME:5050' );
-define('WP_SITEURL', 'http://REPLACEME:5050' );
+define( 'WP_HOME', 'http://REPLACEME:5050' );
+define( 'WP_SITEURL', 'http://REPLACEME:5050' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-
-define('AUTHOR', 'tmarx');
 
 /**#@+
  * Authentication Unique Keys and Salts.
