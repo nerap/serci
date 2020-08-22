e68f8978e5bfb35671b67535f81d7d9b51bcfe8b
nerap <raph.en223@gmail.com>
2020-08-22 04:20:23 +0200

    i hate ft services

--- srcs/containers/nginx/srcs/start.sh
+++ srcs/containers/nginx/srcs/start.sh
@@ -1,13 +1,12 @@
 sh /bin/startscript/start-telegraf.sh
-passwd ybayart << PASSWORD
-motdepasse
-motdepasse
+passwd sshuser << PASSWORD
+pass
+pass
 PASSWORD
 
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 mkdir -p /var/run/sshd
-
 mkdir /run/nginx
 nginx
 php-fpm7
