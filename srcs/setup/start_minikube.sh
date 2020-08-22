ac4034e39ac5a20cdc09211387c8817a8717990e
Raphael COHEN <racohen@e1r11p6.42.fr>
2020-08-23 01:02:21 +0200

    salut

--- srcs/setup/start_minikube.sh
+++ srcs/setup/start_minikube.sh
@@ -1,5 +1,4 @@
 if [[ $(minikube status | grep -c "Running") == 0 ]];then
 	minikube start --vm-driver="docker" --cpus=2 --memory=5000m > /tmp/tp
 	minikube addons enable dashboard >> /tmp/tp
-	minikube addons enable metallb >> /tmp/tp
 fi
