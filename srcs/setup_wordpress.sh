8af0192e2131fdf2801e85364f245880429628fd
nerap <raph.en223@gmail.com>
2020-08-22 03:26:43 +0200

    i hate ft services

--- srcs/setup_wordpress.sh
+++ srcs/setup_wordpress.sh
@@ -1,17 +1,9 @@
 echo "Waiting for Address before launch wordpress"
-while true
+IPWP=`kubectl get services/wordpress -o=custom-columns='ADDRESS:status.loadBalancer.ingress[0].ip'|tail -n1`
+while [ $IPWP = '<none>' ]
 do
-	if [ `kubectl exec deploy/wordpress -- grep -H "REPLACEME" /www/wp-config.php` ]
-	then
-		IPWP=`kubectl get services/wordpress -o=custom-columns='ADDRESS:status.loadBalancer.ingress[0].ip'|tail -n1`
-		while [ $IPWP = '<none>' ]
-		do
-			sleep 1
-			IPWP=`kubectl get services/wordpress -o=custom-columns='ADDRESS:status.loadBalancer.ingress[0].ip'|tail -n1`
-		done
-
-		kubectl exec deploy/wordpress -- sed -ie "s/REPLACEME/$IPWP/g" /www/wp-config.php
-		break
-	fi
-	sleep 10
+	sleep 1
+	IPWP=`kubectl get services/wordpress -o=custom-columns='ADDRESS:status.loadBalancer.ingress[0].ip'|tail -n1`
 done
+kubectl exec deploy/wordpress -- sed -ie "s/REPLACEME/$IPWP/g" /www/wp-config.php
+break
